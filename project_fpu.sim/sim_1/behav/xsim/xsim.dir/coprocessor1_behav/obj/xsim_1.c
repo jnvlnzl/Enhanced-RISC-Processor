9e0ea299f657b8bca523703c25925f3000b18cfc
Jana Valenzuela <102937800+jnvlnzl@users.noreply.github.com>
2025-03-18 17:51:01 +0800

    Add files via upload

---
+++ project_fpu.sim/sim_1/behav/xsim/xsim.dir/coprocessor1_behav/obj/xsim_1.c
@@ -0,0 +1,194 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2020 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+#if defined(_WIN32)
+ #include "stdio.h"
+ #define IKI_DLLESPEC __declspec(dllimport)
+#else
+ #define IKI_DLLESPEC
+#endif
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2020 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+#if defined(_WIN32)
+ #include "stdio.h"
+ #define IKI_DLLESPEC __declspec(dllimport)
+#else
+ #define IKI_DLLESPEC
+#endif
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+IKI_DLLESPEC extern void execute_222(char*, char *);
+IKI_DLLESPEC extern void execute_228(char*, char *);
+IKI_DLLESPEC extern void execute_1537(char*, char *);
+IKI_DLLESPEC extern void execute_1538(char*, char *);
+IKI_DLLESPEC extern void execute_1539(char*, char *);
+IKI_DLLESPEC extern void execute_1540(char*, char *);
+IKI_DLLESPEC extern void execute_1541(char*, char *);
+IKI_DLLESPEC extern void execute_1542(char*, char *);
+IKI_DLLESPEC extern void execute_1543(char*, char *);
+IKI_DLLESPEC extern void execute_1544(char*, char *);
+IKI_DLLESPEC extern void execute_1545(char*, char *);
+IKI_DLLESPEC extern void execute_1546(char*, char *);
+IKI_DLLESPEC extern void execute_1547(char*, char *);
+IKI_DLLESPEC extern void execute_1548(char*, char *);
+IKI_DLLESPEC extern void execute_1549(char*, char *);
+IKI_DLLESPEC extern void execute_1550(char*, char *);
+IKI_DLLESPEC extern void execute_1551(char*, char *);
+IKI_DLLESPEC extern void execute_1552(char*, char *);
+IKI_DLLESPEC extern void execute_1553(char*, char *);
+IKI_DLLESPEC extern void execute_1554(char*, char *);
+IKI_DLLESPEC extern void execute_1555(char*, char *);
+IKI_DLLESPEC extern void execute_1556(char*, char *);
+IKI_DLLESPEC extern void execute_1557(char*, char *);
+IKI_DLLESPEC extern void execute_3(char*, char *);
+IKI_DLLESPEC extern void execute_5(char*, char *);
+IKI_DLLESPEC extern void execute_9(char*, char *);
+IKI_DLLESPEC extern void execute_11(char*, char *);
+IKI_DLLESPEC extern void execute_15(char*, char *);
+IKI_DLLESPEC extern void execute_869(char*, char *);
+IKI_DLLESPEC extern void execute_870(char*, char *);
+IKI_DLLESPEC extern void execute_871(char*, char *);
+IKI_DLLESPEC extern void execute_872(char*, char *);
+IKI_DLLESPEC extern void execute_873(char*, char *);
+IKI_DLLESPEC extern void execute_874(char*, char *);
+IKI_DLLESPEC extern void execute_875(char*, char *);
+IKI_DLLESPEC extern void execute_876(char*, char *);
+IKI_DLLESPEC extern void execute_877(char*, char *);
+IKI_DLLESPEC extern void execute_878(char*, char *);
+IKI_DLLESPEC extern void vlog_const_rhs_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+IKI_DLLESPEC extern void execute_880(char*, char *);
+IKI_DLLESPEC extern void execute_881(char*, char *);
+IKI_DLLESPEC extern void execute_882(char*, char *);
+IKI_DLLESPEC extern void execute_20(char*, char *);
+IKI_DLLESPEC extern void execute_229(char*, char *);
+IKI_DLLESPEC extern void execute_235(char*, char *);
+IKI_DLLESPEC extern void execute_236(char*, char *);
+IKI_DLLESPEC extern void execute_237(char*, char *);
+IKI_DLLESPEC extern void execute_238(char*, char *);
+IKI_DLLESPEC extern void execute_239(char*, char *);
+IKI_DLLESPEC extern void execute_230(char*, char *);
+IKI_DLLESPEC extern void execute_231(char*, char *);
+IKI_DLLESPEC extern void execute_232(char*, char *);
+IKI_DLLESPEC extern void execute_233(char*, char *);
+IKI_DLLESPEC extern void execute_234(char*, char *);
+IKI_DLLESPEC extern void execute_240(char*, char *);
+IKI_DLLESPEC extern void execute_241(char*, char *);
+IKI_DLLESPEC extern void execute_242(char*, char *);
+IKI_DLLESPEC extern void execute_243(char*, char *);
+IKI_DLLESPEC extern void execute_244(char*, char *);
+IKI_DLLESPEC extern void execute_245(char*, char *);
+IKI_DLLESPEC extern void execute_246(char*, char *);
+IKI_DLLESPEC extern void execute_247(char*, char *);
+IKI_DLLESPEC extern void execute_248(char*, char *);
+IKI_DLLESPEC extern void execute_849(char*, char *);
+IKI_DLLESPEC extern void execute_855(char*, char *);
+IKI_DLLESPEC extern void execute_856(char*, char *);
+IKI_DLLESPEC extern void execute_857(char*, char *);
+IKI_DLLESPEC extern void execute_858(char*, char *);
+IKI_DLLESPEC extern void execute_859(char*, char *);
+IKI_DLLESPEC extern void execute_120(char*, char *);
+IKI_DLLESPEC extern void execute_221(char*, char *);
+IKI_DLLESPEC extern void execute_224(char*, char *);
+IKI_DLLESPEC extern void execute_225(char*, char *);
+IKI_DLLESPEC extern void execute_226(char*, char *);
+IKI_DLLESPEC extern void execute_227(char*, char *);
+IKI_DLLESPEC extern void execute_1558(char*, char *);
+IKI_DLLESPEC extern void execute_1559(char*, char *);
+IKI_DLLESPEC extern void execute_1560(char*, char *);
+IKI_DLLESPEC extern void execute_1561(char*, char *);
+IKI_DLLESPEC extern void execute_1562(char*, char *);
+IKI_DLLESPEC extern void execute_1563(char*, char *);
+IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+funcp funcTab[82] = {(funcp)execute_222, (funcp)execute_228, (funcp)execute_1537, (funcp)execute_1538, (funcp)execute_1539, (funcp)execute_1540, (funcp)execute_1541, (funcp)execute_1542, (funcp)execute_1543, (funcp)execute_1544, (funcp)execute_1545, (funcp)execute_1546, (funcp)execute_1547, (funcp)execute_1548, (funcp)execute_1549, (funcp)execute_1550, (funcp)execute_1551, (funcp)execute_1552, (funcp)execute_1553, (funcp)execute_1554, (funcp)execute_1555, (funcp)execute_1556, (funcp)execute_1557, (funcp)execute_3, (funcp)execute_5, (funcp)execute_9, (funcp)execute_11, (funcp)execute_15, (funcp)execute_869, (funcp)execute_870, (funcp)execute_871, (funcp)execute_872, (funcp)execute_873, (funcp)execute_874, (funcp)execute_875, (funcp)execute_876, (funcp)execute_877, (funcp)execute_878, (funcp)vlog_const_rhs_process_execute_0_fast_no_reg_no_agg, (funcp)execute_880, (funcp)execute_881, (funcp)execute_882, (funcp)execute_20, (funcp)execute_229, (funcp)execute_235, (funcp)execute_236, (funcp)execute_237, (funcp)execute_238, (funcp)execute_239, (funcp)execute_230, (funcp)execute_231, (funcp)execute_232, (funcp)execute_233, (funcp)execute_234, (funcp)execute_240, (funcp)execute_241, (funcp)execute_242, (funcp)execute_243, (funcp)execute_244, (funcp)execute_245, (funcp)execute_246, (funcp)execute_247, (funcp)execute_248, (funcp)execute_849, (funcp)execute_855, (funcp)execute_856, (funcp)execute_857, (funcp)execute_858, (funcp)execute_859, (funcp)execute_120, (funcp)execute_221, (funcp)execute_224, (funcp)execute_225, (funcp)execute_226, (funcp)execute_227, (funcp)execute_1558, (funcp)execute_1559, (funcp)execute_1560, (funcp)execute_1561, (funcp)execute_1562, (funcp)execute_1563, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 82;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/coprocessor1_behav/xsim.reloc",  (void **)funcTab, 82);
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/coprocessor1_behav/xsim.reloc");
+}
+
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+
+void wrapper_func_0(char *dp)
+
+{
+
+}
+
+void simulate(char *dp)
+{
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/coprocessor1_behav/xsim.reloc");
+	wrapper_func_0(dp);
+
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_xsimdir_location_if_remapped(argc, argv)  ;
+    iki_set_sv_type_file_path_name("xsim.dir/coprocessor1_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/coprocessor1_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/coprocessor1_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, (void*)0, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
